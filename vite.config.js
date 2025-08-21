2a7c6425ab7affc9b0be866bfa029e365d5639db
pannawat7 <marchza3535@gmail.com>
2025-08-21 12:58:20 +0700

    node update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: './postcss.config.cjs'
+    transformer: 'postcss'
   }
 })
